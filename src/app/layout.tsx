12c89e7d992961025a9a5b4c14a72f7311a3e51d
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-28 22:14:13 +0200

    eslint wanted me to do this, fonts not loading was not even a problem but there you go

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,16 +29,6 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body>{children}</body>
     </html>
