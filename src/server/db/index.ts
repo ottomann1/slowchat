ffcb77f5715f1cc7e91b81d807b90d19e7935e67
Otto <otto.kostmann@hotmail.com>
2024-11-05 18:10:39 +0100

    connectionstring changes reverted

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,16 +2,21 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
+import "dotenv/config";
 
+// Load environment variables"drizzle url",  from .env file
 console.log("drizzleindex url", process.env.POSTGRES_URL)
+
 console.log("drizzleindex user", process.env.POSTGRES_USER)
 console.log("drizzleindex host", process.env.POSTGRES_HOST)
 console.log("drizzleindex database", process.env.POSTGRES_DATABASE)
 console.log("drizzleindex password", process.env.POSTGRES_PASSWORD)
-console.log("drizzleindex public url", process.env.NEXT_PUBLIC_POSTGRES_PASSWORD)
-
 const pool = new Pool({
-  connectionString: "postgres://postgres:party@34.88.234.134:5432/postgres",
+  user: "postgres",
+  host: "34.88.234.134",
+  database: "postgres",
+  password: "party",
+  port: 5432,
 });
 
 const db = drizzle(pool, { schema });
