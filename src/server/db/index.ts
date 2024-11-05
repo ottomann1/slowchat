73c8d56336f105b70dc09317b085b4cd4ac52e4a
Otto <otto.kostmann@hotmail.com>
2024-11-05 17:07:50 +0100

    removed dotenvconfig

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,7 +2,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
-import "dotenv/config";
 
 console.log("drizzleindex url", process.env.POSTGRES_URL)
 console.log("drizzleindex user", process.env.POSTGRES_USER)
@@ -10,11 +9,7 @@ console.log("drizzleindex host", process.env.POSTGRES_HOST)
 console.log("drizzleindex database", process.env.POSTGRES_DATABASE)
 console.log("drizzleindex password", process.env.POSTGRES_PASSWORD)
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 const db = drizzle(pool, { schema });
