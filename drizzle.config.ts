8dc07d2f29599dd1c3880080847b7a393e7aa9e7
Otto <otto.kostmann@hotmail.com>
2024-11-05 16:25:44 +0100

    logs to see if env variables loading correctly

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'drizzle-kit';
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log(process.env.POSTGRES_URL)
+
+console.log(process.env.POSTGRES_USER)
+console.log(process.env.POSTGRES_HOST)
+console.log(process.env.POSTGRES_DATABASE)
+console.log(process.env.POSTGRES_PASSWORD)
+
 
 export default defineConfig({
   dialect: "postgresql",
